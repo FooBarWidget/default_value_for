c9f00f2b67223a8171e0eb1164b8cee0673c7ce4
mgawlista <mg@reqorder.com>
2012-05-14 21:02:57 +0200

    Replaces tabs by spaces

--- lib/default_value_for.rb
+++ lib/default_value_for.rb
@@ -19,108 +19,108 @@
 # THE SOFTWARE.
 
 module DefaultValueFor
-	class NormalValueContainer
-		def initialize(value)
-			@value = value
-		end
+  class NormalValueContainer
+    def initialize(value)
+      @value = value
+    end
 
-		def evaluate(instance)
-			if @value.duplicable?
-				return @value.dup
-			else
-				return @value
-			end
-		end
-	end
+    def evaluate(instance)
+      if @value.duplicable?
+        return @value.dup
+      else
+        return @value
+      end
+    end
+  end
 
-	class BlockValueContainer
-		def initialize(block)
-			@block = block
-		end
+  class BlockValueContainer
+    def initialize(block)
+      @block = block
+    end
 
-		def evaluate(instance)
-			if @block.arity == 0
-				return @block.call
-			else
-				return @block.call(instance)
-			end
-		end
-	end
+    def evaluate(instance)
+      if @block.arity == 0
+        return @block.call
+      else
+        return @block.call(instance)
+      end
+    end
+  end
 
-	module ClassMethods
-	  # Declares a default value for the given attribute.
-	  #
-	  # Sets the default value to the given options parameter unless the given options equal { :value => ... }
-	  #
-	  # The <tt>options</tt> can be used to specify the following things:
+  module ClassMethods
+    # Declares a default value for the given attribute.
+    #
+    # Sets the default value to the given options parameter unless the given options equal { :value => ... }
+    #
+    # The <tt>options</tt> can be used to specify the following things:
     # * <tt>value</tt> - Sets the default value.
     # * <tt>allows_nil (default: true)</tt> - Sets explicitly passed nil values if option is set to true.
-		def default_value_for(attribute, options = {}, &block)
-		  value = options.is_a?(Hash) && options.stringify_keys.has_key?('value') ? options.stringify_keys['value'] : options
-		  allows_nil = options.is_a?(Hash) && options.stringify_keys.has_key?('allows_nil') ? options.stringify_keys['allows_nil'] : true
-		  
-			if !method_defined?(:set_default_values)
-				include(InstanceMethods)
+    def default_value_for(attribute, options = {}, &block)
+      value = options.is_a?(Hash) && options.stringify_keys.has_key?('value') ? options.stringify_keys['value'] : options
+      allows_nil = options.is_a?(Hash) && options.stringify_keys.has_key?('allows_nil') ? options.stringify_keys['allows_nil'] : true
+      
+      if !method_defined?(:set_default_values)
+        include(InstanceMethods)
 
-				after_initialize :set_default_values
-				
-				if respond_to?(:class_attribute)
-					class_attribute :_default_attribute_values
-					class_attribute :_default_attribute_values_not_allowing_nil
-				else
-					class_inheritable_accessor :_default_attribute_values
-					class_inheritable_accessor :_default_attribute_values_not_allowing_nil
-				end
-				
-				extend(DelayedClassMethods)
-				init_hash = true
-			end
-			if init_hash || !singleton_methods(false).to_s.include?("_default_attribute_values")
-				self._default_attribute_values = ActiveSupport::OrderedHash.new
-				self._default_attribute_values_not_allowing_nil = []
-			end
-			if block_given?
-				container = BlockValueContainer.new(block)
-			else
-				container = NormalValueContainer.new(value)
-			end
-			_default_attribute_values[attribute.to_s] = container
-			_default_attribute_values_not_allowing_nil << attribute.to_s unless allows_nil
-		end
+        after_initialize :set_default_values
+        
+        if respond_to?(:class_attribute)
+          class_attribute :_default_attribute_values
+          class_attribute :_default_attribute_values_not_allowing_nil
+        else
+          class_inheritable_accessor :_default_attribute_values
+          class_inheritable_accessor :_default_attribute_values_not_allowing_nil
+        end
+        
+        extend(DelayedClassMethods)
+        init_hash = true
+      end
+      if init_hash || !singleton_methods(false).to_s.include?("_default_attribute_values")
+        self._default_attribute_values = ActiveSupport::OrderedHash.new
+        self._default_attribute_values_not_allowing_nil = []
+      end
+      if block_given?
+        container = BlockValueContainer.new(block)
+      else
+        container = NormalValueContainer.new(value)
+      end
+      _default_attribute_values[attribute.to_s] = container
+      _default_attribute_values_not_allowing_nil << attribute.to_s unless allows_nil
+    end
 
-		def default_values(values)
-			values.each_pair do |key, options|
-			  options = options.stringify_keys if options.is_a?(Hash)
-			  
-			  value = options.is_a?(Hash) && options.has_key?('value') ? options['value'] : options
-			  
-				if value.kind_of? Proc
-					default_value_for(key, options.is_a?(Hash) ? options : {}, &value)
-				else
-					default_value_for(key, options)
-				end
-			end
-		end
-	end
+    def default_values(values)
+      values.each_pair do |key, options|
+        options = options.stringify_keys if options.is_a?(Hash)
+        
+        value = options.is_a?(Hash) && options.has_key?('value') ? options['value'] : options
+        
+        if value.kind_of? Proc
+          default_value_for(key, options.is_a?(Hash) ? options : {}, &value)
+        else
+          default_value_for(key, options)
+        end
+      end
+    end
+  end
 
-	module DelayedClassMethods
-		def _all_default_attribute_values
-			return _default_attribute_values unless superclass.respond_to?(:_default_attribute_values)
-			superclass._all_default_attribute_values.merge(_default_attribute_values)
-		end
-		
-		def _all_default_attribute_values_not_allowing_nil
+  module DelayedClassMethods
+    def _all_default_attribute_values
+      return _default_attribute_values unless superclass.respond_to?(:_default_attribute_values)
+      superclass._all_default_attribute_values.merge(_default_attribute_values)
+    end
+    
+    def _all_default_attribute_values_not_allowing_nil
       return _default_attribute_values_not_allowing_nil unless superclass.respond_to?(:_default_attribute_values_not_allowing_nil)
       superclass._all_default_attribute_values_not_allowing_nil.merge(_default_attribute_values_not_allowing_nil)
     end
-	end
+  end
 
-	module InstanceMethods
-	  def initialize(attributes = nil, options = {})
-	    @initialization_attributes = attributes.is_a?(Hash) ? attributes.stringify_keys : {}
-	    
-	    unless options[:without_protection]
-	      if respond_to?(:mass_assignment_options) && options.has_key?(:as)
+  module InstanceMethods
+    def initialize(attributes = nil, options = {})
+      @initialization_attributes = attributes.is_a?(Hash) ? attributes.stringify_keys : {}
+      
+      unless options[:without_protection]
+        if respond_to?(:mass_assignment_options) && options.has_key?(:as)
           @initialization_attributes = sanitize_for_mass_assignment(@initialization_attributes, options[:as])
         elsif respond_to? :sanitize_for_mass_assignment
           @initialization_attributes = sanitize_for_mass_assignment(@initialization_attributes)
@@ -128,10 +128,10 @@ module DefaultValueFor
           @initialization_attributes = remove_attributes_protected_from_mass_assignment(@initialization_attributes)
         end
       end
-	    
-	    super(attributes, options)
-	  end
-	  
+      
+      super(attributes, options)
+    end
+    
     def set_default_values
       self.class._all_default_attribute_values.each do |attribute, container|
         next unless self.new_record? || self.class._all_default_attribute_values_not_allowing_nil.include?(attribute)
@@ -147,12 +147,12 @@ module DefaultValueFor
         changed_attributes.delete(attribute)
       end
     end
-	end
+  end
 end
 
 if defined?(Rails::Railtie)
-	require 'default_value_for/railtie'
+  require 'default_value_for/railtie'
 else
-	# Rails 2 initialization
-	ActiveRecord::Base.extend(DefaultValueFor::ClassMethods)
+  # Rails 2 initialization
+  ActiveRecord::Base.extend(DefaultValueFor::ClassMethods)
 end
--- test.rb
+++ test.rb
@@ -24,7 +24,7 @@ require 'rubygems'
 gem 'rails', want_rails_version
 gem 'activerecord', want_rails_version
 begin
-	require 'rails/railtie'
+  require 'rails/railtie'
 rescue LoadError
 end
 require 'active_record'
@@ -36,92 +36,92 @@ require 'default_value_for'
 Dir.chdir(File.dirname(__FILE__))
 
 if RUBY_PLATFORM == "java"
-	database_adapter = "jdbcsqlite3"
+  database_adapter = "jdbcsqlite3"
 else
-	database_adapter = "sqlite3"
+  database_adapter = "sqlite3"
 end
 
 File.unlink('test.sqlite3') rescue nil
 ActiveRecord::Base.logger = Logger.new(STDERR)
 ActiveRecord::Base.logger.level = Logger::WARN
 ActiveRecord::Base.establish_connection(
-	:adapter => database_adapter,
-	:database => 'test.sqlite3'
+  :adapter => database_adapter,
+  :database => 'test.sqlite3'
 )
 ActiveRecord::Base.connection.create_table(:users, :force => true) do |t|
-	t.string :username
-	t.integer :default_number
+  t.string :username
+  t.integer :default_number
 end
 ActiveRecord::Base.connection.create_table(:numbers, :force => true) do |t|
-	t.string :type
-	t.integer :number
-	t.integer :count, :null => false, :default => 1
-	t.integer :user_id
-	t.timestamp :timestamp
+  t.string :type
+  t.integer :number
+  t.integer :count, :null => false, :default => 1
+  t.integer :user_id
+  t.timestamp :timestamp
 end
 
 if defined?(Rails::Railtie)
-	DefaultValueFor.initialize_railtie
-	DefaultValueFor.initialize_active_record_extensions
+  DefaultValueFor.initialize_railtie
+  DefaultValueFor.initialize_active_record_extensions
 end
 
 class User < ActiveRecord::Base
-	has_many :numbers, :class_name => 'TestClass'
+  has_many :numbers, :class_name => 'TestClass'
 end
 
 class Number < ActiveRecord::Base
 end
 
 class DefaultValuePluginTest < Test::Unit::TestCase
-	def setup
-		Number.create(:number => 9876)
-	end
+  def setup
+    Number.create(:number => 9876)
+  end
 
-	def teardown
-		Number.delete_all
-	end
+  def teardown
+    Number.delete_all
+  end
 
-	def define_model_class(name = "TestClass", parent_class_name = "ActiveRecord::Base", &block)
-		Object.send(:remove_const, name) rescue nil
-		eval("class #{name} < #{parent_class_name}; end", TOPLEVEL_BINDING)
-		klass = eval(name, TOPLEVEL_BINDING)
-		klass.class_eval do
-			set_table_name 'numbers'
-		end
-		klass.class_eval(&block) if block_given?
-	end
+  def define_model_class(name = "TestClass", parent_class_name = "ActiveRecord::Base", &block)
+    Object.send(:remove_const, name) rescue nil
+    eval("class #{name} < #{parent_class_name}; end", TOPLEVEL_BINDING)
+    klass = eval(name, TOPLEVEL_BINDING)
+    klass.class_eval do
+      set_table_name 'numbers'
+    end
+    klass.class_eval(&block) if block_given?
+  end
 
-	def test_default_value_can_be_passed_as_argument
-		define_model_class do
-			default_value_for(:number, 1234)
-		end
-		object = TestClass.new
-		assert_equal 1234, object.number
-	end
+  def test_default_value_can_be_passed_as_argument
+    define_model_class do
+      default_value_for(:number, 1234)
+    end
+    object = TestClass.new
+    assert_equal 1234, object.number
+  end
 
-	def test_default_value_can_be_passed_as_block
-		define_model_class do
-			default_value_for(:number) { 1234 }
-		end
-		object = TestClass.new
-		assert_equal 1234, object.number
-	end
+  def test_default_value_can_be_passed_as_block
+    define_model_class do
+      default_value_for(:number) { 1234 }
+    end
+    object = TestClass.new
+    assert_equal 1234, object.number
+  end
 
-	def test_works_with_create
-		define_model_class do
-			default_value_for :number, 1234
-		end
-		
-		object = TestClass.create
-		assert_not_nil TestClass.find_by_number(1234)
-		
-		# allows nil for existing records
-		object.update_attribute(:number, nil)
-		assert_nil TestClass.find_by_number(1234)
-		assert_nil TestClass.find(object.id).number
-	end
+  def test_works_with_create
+    define_model_class do
+      default_value_for :number, 1234
+    end
+    
+    object = TestClass.create
+    assert_not_nil TestClass.find_by_number(1234)
+    
+    # allows nil for existing records
+    object.update_attribute(:number, nil)
+    assert_nil TestClass.find_by_number(1234)
+    assert_nil TestClass.find(object.id).number
+  end
 
-	def test_does_not_allow_nil_for_existing_record
+  def test_does_not_allow_nil_for_existing_record
     define_model_class do
       default_value_for(:number, :allows_nil => false) { 1234 }
     end
@@ -132,43 +132,43 @@ class DefaultValuePluginTest < Test::Unit::TestCase
     object.update_attribute(:number, nil)
     assert_nil TestClass.find_by_number(1234)
     assert_equal 1234, TestClass.find(object.id).number
-	end
+  end
 
- 	def test_overwrites_db_default
-		define_model_class do
-			default_value_for :count, 1234
-		end
-		object = TestClass.new
-		assert_equal 1234, object.count
-	end
+   def test_overwrites_db_default
+    define_model_class do
+      default_value_for :count, 1234
+    end
+    object = TestClass.new
+    assert_equal 1234, object.count
+  end
 
-	def test_doesnt_overwrite_values_provided_by_mass_assignment
-		define_model_class do
-			default_value_for :number, 1234
-		end
-		object = TestClass.new(:number => 1, :count => 2)
-		assert_equal 1, object.number
-	end
+  def test_doesnt_overwrite_values_provided_by_mass_assignment
+    define_model_class do
+      default_value_for :number, 1234
+    end
+    object = TestClass.new(:number => 1, :count => 2)
+    assert_equal 1, object.number
+  end
 
-	def test_doesnt_overwrite_values_provided_by_multiparameter_assignment
-		define_model_class do
-			default_value_for :timestamp, Time.mktime(2000, 1, 1)
-		end
-		timestamp = Time.mktime(2009, 1, 1)
-		object = TestClass.new('timestamp(1i)' => '2009', 'timestamp(2i)' => '1', 'timestamp(3i)' => '1')
-		assert_equal timestamp, object.timestamp
-	end
+  def test_doesnt_overwrite_values_provided_by_multiparameter_assignment
+    define_model_class do
+      default_value_for :timestamp, Time.mktime(2000, 1, 1)
+    end
+    timestamp = Time.mktime(2009, 1, 1)
+    object = TestClass.new('timestamp(1i)' => '2009', 'timestamp(2i)' => '1', 'timestamp(3i)' => '1')
+    assert_equal timestamp, object.timestamp
+  end
 
-	def test_doesnt_overwrite_values_provided_by_constructor_block
-		define_model_class do
-			default_value_for :number, 1234
-		end
-		object = TestClass.new do |x|
-			x.number = 1
-			x.count = 2
-		end
-		assert_equal 1, object.number
-	end
+  def test_doesnt_overwrite_values_provided_by_constructor_block
+    define_model_class do
+      default_value_for :number, 1234
+    end
+    object = TestClass.new do |x|
+      x.number = 1
+      x.count = 2
+    end
+    assert_equal 1, object.number
+  end
 
   def test_doesnt_overwrite_explicitly_provided_nil_values_in_mass_assignment
     define_model_class do
@@ -178,110 +178,110 @@ class DefaultValuePluginTest < Test::Unit::TestCase
     assert_equal nil, object.number
   end
 
-	def test_overwrites_explicitly_provided_nil_values_in_mass_assignment
-		define_model_class do
-			default_value_for :number, :value => 1234, :allows_nil => false
-		end
-		object = TestClass.new(:number => nil)
-		assert_equal 1234, object.number
-	end
+  def test_overwrites_explicitly_provided_nil_values_in_mass_assignment
+    define_model_class do
+      default_value_for :number, :value => 1234, :allows_nil => false
+    end
+    object = TestClass.new(:number => nil)
+    assert_equal 1234, object.number
+  end
 
-	def test_default_values_are_inherited
-		define_model_class("TestSuperClass") do
-			default_value_for :number, 1234
-		end
-		define_model_class("TestClass", "TestSuperClass")
-		object = TestClass.new
-		assert_equal 1234, object.number
-	end
+  def test_default_values_are_inherited
+    define_model_class("TestSuperClass") do
+      default_value_for :number, 1234
+    end
+    define_model_class("TestClass", "TestSuperClass")
+    object = TestClass.new
+    assert_equal 1234, object.number
+  end
 
-	def test_default_values_in_subclass
-		define_model_class("TestSuperClass") do
-		end
-		define_model_class("TestClass", "TestSuperClass") do
-			default_value_for :number, 5678
-		end
+  def test_default_values_in_subclass
+    define_model_class("TestSuperClass") do
+    end
+    define_model_class("TestClass", "TestSuperClass") do
+      default_value_for :number, 5678
+    end
 
-		object = TestClass.new
-		assert_equal 5678, object.number
+    object = TestClass.new
+    assert_equal 5678, object.number
 
-		object = TestSuperClass.new
-		assert_nil object.number
-	end
+    object = TestSuperClass.new
+    assert_nil object.number
+  end
 
-	def test_multiple_default_values_in_subclass_with_default_values_in_parent_class
-		define_model_class("TestSuperClass") do
-			default_value_for :other_number, nil
-			attr_accessor :other_number
-		end
-		define_model_class("TestClass", "TestSuperClass") do
-			default_value_for :number, 5678
+  def test_multiple_default_values_in_subclass_with_default_values_in_parent_class
+    define_model_class("TestSuperClass") do
+      default_value_for :other_number, nil
+      attr_accessor :other_number
+    end
+    define_model_class("TestClass", "TestSuperClass") do
+      default_value_for :number, 5678
 
-			# Ensure second call in this class doesn't reset _default_attribute_values,
-			# and also doesn't consider the parent class' _default_attribute_values when
-			# making that check.
-			default_value_for :user_id, 9999
-		end
+      # Ensure second call in this class doesn't reset _default_attribute_values,
+      # and also doesn't consider the parent class' _default_attribute_values when
+      # making that check.
+      default_value_for :user_id, 9999
+    end
 
-		object = TestClass.new
-		assert_nil object.other_number
-		assert_equal 5678, object.number
-		assert_equal 9999, object.user_id
-	end
+    object = TestClass.new
+    assert_nil object.other_number
+    assert_equal 5678, object.number
+    assert_equal 9999, object.user_id
+  end
 
-	def test_override_default_values_in_subclass
-		define_model_class("TestSuperClass") do
-			default_value_for :number, 1234
-		end
-		define_model_class("TestClass", "TestSuperClass") do
-			default_value_for :number, 5678
-		end
+  def test_override_default_values_in_subclass
+    define_model_class("TestSuperClass") do
+      default_value_for :number, 1234
+    end
+    define_model_class("TestClass", "TestSuperClass") do
+      default_value_for :number, 5678
+    end
 
-		object = TestClass.new
-		assert_equal 5678, object.number
+    object = TestClass.new
+    assert_equal 5678, object.number
 
-		object = TestSuperClass.new
-		assert_equal 1234, object.number
-	end
+    object = TestSuperClass.new
+    assert_equal 1234, object.number
+  end
 
-	def test_default_values_in_subclass_do_not_affect_parent_class
-		define_model_class("TestSuperClass") do
-			default_value_for :number, 1234
-		end
-		define_model_class("TestClass", "TestSuperClass") do
-			default_value_for :hello, "hi"
-			attr_accessor :hello
-		end
+  def test_default_values_in_subclass_do_not_affect_parent_class
+    define_model_class("TestSuperClass") do
+      default_value_for :number, 1234
+    end
+    define_model_class("TestClass", "TestSuperClass") do
+      default_value_for :hello, "hi"
+      attr_accessor :hello
+    end
 
-		assert_nothing_raised { TestSuperClass.new }
-		assert !TestSuperClass._default_attribute_values.include?(:hello)
-	end
+    assert_nothing_raised { TestSuperClass.new }
+    assert !TestSuperClass._default_attribute_values.include?(:hello)
+  end
 
-	def test_doesnt_set_default_on_saved_records
-		define_model_class do
-			default_value_for :number, 1234
-		end
-		assert_equal 9876, TestClass.find(:first).number
-	end
+  def test_doesnt_set_default_on_saved_records
+    define_model_class do
+      default_value_for :number, 1234
+    end
+    assert_equal 9876, TestClass.find(:first).number
+  end
 
-	def test_also_works_on_attributes_that_arent_database_columns
-		define_model_class do
-			default_value_for :hello, "hi"
-			attr_accessor :hello
-		end
-		object = TestClass.new
-		assert_equal 'hi', object.hello
-	end
+  def test_also_works_on_attributes_that_arent_database_columns
+    define_model_class do
+      default_value_for :hello, "hi"
+      attr_accessor :hello
+    end
+    object = TestClass.new
+    assert_equal 'hi', object.hello
+  end
 
-	def test_constructor_ignores_forbidden_mass_assignment_attributes
-		define_model_class do
-			default_value_for :number, 1234
-			attr_protected :number
-		end
-		object = TestClass.new(:number => 5678, :count => 987)
-		assert_equal 1234, object.number
-		assert_equal 987, object.count
-	end
+  def test_constructor_ignores_forbidden_mass_assignment_attributes
+    define_model_class do
+      default_value_for :number, 1234
+      attr_protected :number
+    end
+    object = TestClass.new(:number => 5678, :count => 987)
+    assert_equal 1234, object.number
+    assert_equal 987, object.count
+  end
 
   def test_constructor_respects_without_protection_option
     define_model_class do
@@ -301,115 +301,115 @@ class DefaultValuePluginTest < Test::Unit::TestCase
     end
   end
 
-	def test_doesnt_conflict_with_overrided_initialize_method_in_model_class
-		define_model_class do
-			def initialize(attrs = {})
-				@initialized = true
-				super(:count => 5678)
-			end
+  def test_doesnt_conflict_with_overrided_initialize_method_in_model_class
+    define_model_class do
+      def initialize(attrs = {})
+        @initialized = true
+        super(:count => 5678)
+      end
 
-			default_value_for :number, 1234
-		end
-		object = TestClass.new
-		assert_equal 1234, object.number
-		assert_equal 5678, object.count
-		assert object.instance_variable_get('@initialized')
-	end
+      default_value_for :number, 1234
+    end
+    object = TestClass.new
+    assert_equal 1234, object.number
+    assert_equal 5678, object.count
+    assert object.instance_variable_get('@initialized')
+  end
 
-	def test_model_instance_is_passed_to_the_given_block
-		$instance = nil
-		define_model_class do
-			default_value_for :number do |n|
-				$instance = n
-			end
-		end
-		object = TestClass.new
-		assert_same object, $instance
-	end
+  def test_model_instance_is_passed_to_the_given_block
+    $instance = nil
+    define_model_class do
+      default_value_for :number do |n|
+        $instance = n
+      end
+    end
+    object = TestClass.new
+    assert_same object, $instance
+  end
 
-	def test_can_specify_default_value_via_association
-		user = User.create(:username => 'Kanako', :default_number => 123)
-		define_model_class do
-			belongs_to :user
+  def test_can_specify_default_value_via_association
+    user = User.create(:username => 'Kanako', :default_number => 123)
+    define_model_class do
+      belongs_to :user
 
-			default_value_for :number do |n|
-				n.user.default_number
-			end
-		end
-		object = user.numbers.create
-		assert_equal 123, object.number
-	end
+      default_value_for :number do |n|
+        n.user.default_number
+      end
+    end
+    object = user.numbers.create
+    assert_equal 123, object.number
+  end
 
-	def test_default_values
-		define_model_class do
-			default_values :type => "normal",
-				:number => lambda { 10 + 5 },
-				:timestamp => lambda {|_| Time.now }
-		end
+  def test_default_values
+    define_model_class do
+      default_values :type => "normal",
+        :number => lambda { 10 + 5 },
+        :timestamp => lambda {|_| Time.now }
+    end
 
-		object = TestClass.new
-		assert_equal("normal", object.type)
-		assert_equal(15, object.number)
-	end
+    object = TestClass.new
+    assert_equal("normal", object.type)
+    assert_equal(15, object.number)
+  end
 
-	def test_default_value_order
-		define_model_class do
-			default_value_for :count, 5
-			default_value_for :number do |this|
-				this.count * 2
-			end
-		end
-		object = TestClass.new
-		assert_equal(5, object.count)
-		assert_equal(10, object.number)
-	end
+  def test_default_value_order
+    define_model_class do
+      default_value_for :count, 5
+      default_value_for :number do |this|
+        this.count * 2
+      end
+    end
+    object = TestClass.new
+    assert_equal(5, object.count)
+    assert_equal(10, object.number)
+  end
 
-	def test_attributes_with_default_values_are_not_marked_as_changed
-		define_model_class do
-			default_value_for :count, 5
-			default_value_for :number, 2
-		end
+  def test_attributes_with_default_values_are_not_marked_as_changed
+    define_model_class do
+      default_value_for :count, 5
+      default_value_for :number, 2
+    end
 
-		object = TestClass.new
-		assert(!object.changed?)
-		assert_equal([], object.changed)
+    object = TestClass.new
+    assert(!object.changed?)
+    assert_equal([], object.changed)
 
-		object.type = "foo"
-		assert(object.changed?)
-		assert_equal(["type"], object.changed)
-	end
+    object.type = "foo"
+    assert(object.changed?)
+    assert_equal(["type"], object.changed)
+  end
 
-	def test_default_values_are_duplicated
-		define_model_class do
-			set_table_name "users"
-			default_value_for :username, "hello"
-		end
-		user1 = TestClass.new
-		user1.username << " world"
-		user2 = TestClass.new
-		assert_equal("hello", user2.username)
-	end
+  def test_default_values_are_duplicated
+    define_model_class do
+      set_table_name "users"
+      default_value_for :username, "hello"
+    end
+    user1 = TestClass.new
+    user1.username << " world"
+    user2 = TestClass.new
+    assert_equal("hello", user2.username)
+  end
 
-	def test_default_values_are_shallow_copied
-		define_model_class do
-			set_table_name "users"
-			attr_accessor :hash
-			default_value_for :hash, { 1 => [] }
-		end
-		user1 = TestClass.new
-		user1.hash[1] << 1
-		user2 = TestClass.new
-		assert_equal([1], user2.hash[1])
-	end
-	
-	def test_constructor_does_not_affect_the_hash_passed_to_it
-		define_model_class do
-			default_value_for :count, 5
-		end
+  def test_default_values_are_shallow_copied
+    define_model_class do
+      set_table_name "users"
+      attr_accessor :hash
+      default_value_for :hash, { 1 => [] }
+    end
+    user1 = TestClass.new
+    user1.hash[1] << 1
+    user2 = TestClass.new
+    assert_equal([1], user2.hash[1])
+  end
+  
+  def test_constructor_does_not_affect_the_hash_passed_to_it
+    define_model_class do
+      default_value_for :count, 5
+    end
 
-		options = { :count => 5, :user_id => 1 }
-		options_dup = options.dup
-		object = TestClass.new(options)
-		assert_equal(options_dup, options)
-	end
+    options = { :count => 5, :user_id => 1 }
+    options_dup = options.dup
+    object = TestClass.new(options)
+    assert_equal(options_dup, options)
+  end
 end
