48b79e13838d2bcfe9856018591cc44edc4087ee
Norman Clarke <norman@njclarke.com>
2014-12-08 22:08:23 -0300

    Bump Rails 4.2 version for tests

--- Gemfile.rails.4.2.rb
+++ Gemfile.rails.4.2.rb
@@ -1,4 +1,4 @@
 instance_eval File.read(File.expand_path('../Gemfile', __FILE__))
-gem 'activesupport', '~> 4.2.0.beta.4'
-gem 'activerecord', '~> 4.2.0.beta.4'
-gem 'railties', '~> 4.2.0.beta.4'
+gem 'activesupport', '~> 4.2.0.rc.2'
+gem 'activerecord', '~> 4.2.0.rc.2'
+gem 'railties', '~> 4.2.0.rc.2'
