059b9169c0b54a5df6bb714c6fa5794ff7b1069b
Norman Clarke <norman@njclarke.com>
2014-12-15 12:33:05 -0300

    Use "book" in place of "number"
    `Book#number` looks less silly than `Number#number`

--- test.rb
+++ test.rb
@@ -50,7 +50,7 @@ ActiveRecord::Base.connection.create_table(:users, :force => true) do |t|
   t.integer :default_number
 end
 
-ActiveRecord::Base.connection.create_table(:numbers, :force => true) do |t|
+ActiveRecord::Base.connection.create_table(:books, :force => true) do |t|
   t.string :type
   t.integer :number
   t.integer :count, :null => false, :default => 1
@@ -68,9 +68,9 @@ end
 class DefaultValuePluginTest < TestCaseClass
   def around
     Object.const_set(:User, Class.new(ActiveRecord::Base))
-    Object.const_set(:Number, Class.new(ActiveRecord::Base))
-    User.has_many :numbers
-    Number.belongs_to :user
+    Object.const_set(:Book, Class.new(ActiveRecord::Base))
+    User.has_many :books
+    Book.belongs_to :user
 
     ActiveRecord::Base.transaction do
       yield
@@ -78,7 +78,7 @@ class DefaultValuePluginTest < TestCaseClass
     end
   ensure
     Object.send(:remove_const, :User)
-    Object.send(:remove_const, :Number)
+    Object.send(:remove_const, :Book)
   end
 
   def define_model_class(name = "TestClass", parent_class_name = "ActiveRecord::Base", &block)
@@ -86,13 +86,13 @@ class DefaultValuePluginTest < TestCaseClass
     eval("class #{name} < #{parent_class_name}; end", TOPLEVEL_BINDING)
     klass = eval(name, TOPLEVEL_BINDING)
     klass.class_eval do
-      self.table_name = 'numbers'
+      self.table_name = 'books'
     end
     klass.class_eval(&block) if block_given?
   end
 
   def test_default_value_on_attribute_methods
-    Number.class_eval do
+    Book.class_eval do
       serialize :stuff
       default_value_for :color, :green
       def color; (self.stuff || {})[:color]; end
@@ -101,67 +101,67 @@ class DefaultValuePluginTest < TestCaseClass
         self.stuff[:color] = val
       end
     end
-    object = Number.create
+    object = Book.create
     assert_equal :green, object.color
   end
 
   def test_default_value_can_be_passed_as_argument
-    Number.default_value_for(:number, 1234)
-    object = Number.new
+    Book.default_value_for(:number, 1234)
+    object = Book.new
     assert_equal 1234, object.number
   end
 
   def test_default_value_can_be_passed_as_block
-    Number.default_value_for(:number) { 1234 }
-    object = Number.new
+    Book.default_value_for(:number) { 1234 }
+    object = Book.new
     assert_equal 1234, object.number
   end
 
   def test_works_with_create
-    Number.default_value_for :number, 1234
+    Book.default_value_for :number, 1234
 
-    object = Number.create
-    refute_nil Number.find_by_number(1234)
+    object = Book.create
+    refute_nil Book.find_by_number(1234)
 
     # allows nil for existing records
     object.update_attribute(:number, nil)
-    assert_nil Number.find_by_number(1234)
-    assert_nil Number.find(object.id).number
+    assert_nil Book.find_by_number(1234)
+    assert_nil Book.find(object.id).number
   end
 
   def test_does_not_allow_nil_for_existing_record
-    Number.default_value_for(:number, :allows_nil => false) { 1234 }
+    Book.default_value_for(:number, :allows_nil => false) { 1234 }
 
-    object = Number.create
+    object = Book.create
 
     # allows nil for existing records
     object.update_attribute(:number, nil)
-    assert_nil Number.find_by_number(1234)
-    assert_equal 1234, Number.find(object.id).number
+    assert_nil Book.find_by_number(1234)
+    assert_equal 1234, Book.find(object.id).number
   end
 
   def test_overwrites_db_default
-    Number.default_value_for :count, 1234
-    object = Number.new
+    Book.default_value_for :count, 1234
+    object = Book.new
     assert_equal 1234, object.count
   end
 
   def test_doesnt_overwrite_values_provided_by_mass_assignment
-    Number.default_value_for :number, 1234
-    object = Number.new(:number => 1, :count => 2)
+    Book.default_value_for :number, 1234
+    object = Book.new(:number => 1, :count => 2)
     assert_equal 1, object.number
   end
 
   def test_doesnt_overwrite_values_provided_by_multiparameter_assignment
-    Number.default_value_for :timestamp, Time.mktime(2000, 1, 1)
+    Book.default_value_for :timestamp, Time.mktime(2000, 1, 1)
     timestamp = Time.mktime(2009, 1, 1)
-    object = Number.new('timestamp(1i)' => '2009', 'timestamp(2i)' => '1', 'timestamp(3i)' => '1')
+    object = Book.new('timestamp(1i)' => '2009', 'timestamp(2i)' => '1', 'timestamp(3i)' => '1')
     assert_equal timestamp, object.timestamp
   end
 
   def test_doesnt_overwrite_values_provided_by_constructor_block
-    Number.default_value_for :number, 1234
-    object = Number.new do |x|
+    Book.default_value_for :number, 1234
+    object = Book.new do |x|
       x.number = 1
       x.count = 2
     end
@@ -169,14 +169,14 @@ class DefaultValuePluginTest < TestCaseClass
   end
 
   def test_doesnt_overwrite_explicitly_provided_nil_values_in_mass_assignment
-    Number.default_value_for :number, 1234
-    object = Number.new(:number => nil)
+    Book.default_value_for :number, 1234
+    object = Book.new(:number => nil)
     assert_equal nil, object.number
   end
 
   def test_overwrites_explicitly_provided_nil_values_in_mass_assignment
-    Number.default_value_for :number, :value => 1234, :allows_nil => false
-    object = Number.new(:number => nil)
+    Book.default_value_for :number, :value => 1234, :allows_nil => false
+    object = Book.new(:number => nil)
     assert_equal 1234, object.number
   end
 
@@ -264,33 +264,33 @@ class DefaultValuePluginTest < TestCaseClass
   end
 
   def test_doesnt_set_default_on_saved_records
-    Number.create(:number => 9876)
-    Number.default_value_for :number, 1234
-    assert_equal 9876, Number.first.number
+    Book.create(:number => 9876)
+    Book.default_value_for :number, 1234
+    assert_equal 9876, Book.first.number
   end
 
   def test_also_works_on_attributes_that_arent_database_columns
-    Number.class_eval do
+    Book.class_eval do
       default_value_for :hello, "hi"
       attr_accessor :hello
     end
-    object = Number.new
+    object = Book.new
     assert_equal 'hi', object.hello
   end
 
   if ActiveRecord::VERSION::MAJOR < 4
     def test_constructor_ignores_forbidden_mass_assignment_attributes
-      Number.class_eval do
+      Book.class_eval do
         default_value_for :number, 1234
         attr_protected :number
       end
-      object = Number.new(:number => 5678, :count => 987)
+      object = Book.new(:number => 5678, :count => 987)
       assert_equal 1234, object.number
       assert_equal 987, object.count
     end
 
     def test_constructor_respects_without_protection_option
-      Number.class_eval do
+      Book.class_eval do
         default_value_for :number, 1234
         attr_protected :number
 
@@ -299,9 +299,9 @@ class DefaultValuePluginTest < TestCaseClass
         end
       end
 
-      if Number.new.respond_to_mass_assignment_options?
+      if Book.new.respond_to_mass_assignment_options?
         # test without protection feature if available in current ActiveRecord version
-        object = Number.create!({:number => 5678, :count => 987}, :without_protection => true)
+        object = Book.create!({:number => 5678, :count => 987}, :without_protection => true)
         assert_equal 5678, object.number
         assert_equal 987, object.count
       end
@@ -309,7 +309,7 @@ class DefaultValuePluginTest < TestCaseClass
   end
 
   def test_doesnt_conflict_with_overrided_initialize_method_in_model_class
-    Number.class_eval do
+    Book.class_eval do
       def initialize(attrs = {})
         @initialized = true
         super(:count => 5678)
@@ -317,7 +317,7 @@ class DefaultValuePluginTest < TestCaseClass
 
       default_value_for :number, 1234
     end
-    object = Number.new
+    object = Book.new
     assert_equal 1234, object.number
     assert_equal 5678, object.count
     assert object.instance_variable_get('@initialized')
@@ -325,49 +325,49 @@ class DefaultValuePluginTest < TestCaseClass
 
   def test_model_instance_is_passed_to_the_given_block
     $instance = nil
-    Number.default_value_for :number do |n|
+    Book.default_value_for :number do |n|
       $instance = n
     end
-    object = Number.new
+    object = Book.new
     assert_same object, $instance
   end
 
   def test_can_specify_default_value_via_association
     user = User.create(:username => 'Kanako', :default_number => 123)
-    Number.default_value_for :number do |n|
+    Book.default_value_for :number do |n|
       n.user.default_number
     end
-    object = user.numbers.create
+    object = user.books.create
     assert_equal 123, object.number
   end
 
   def test_default_values
-    Number.default_values({
+    Book.default_values({
       :type      => "normal",
       :number    => lambda { 10 + 5 },
       :timestamp => lambda {|_| Time.now }
     })
 
-    object = Number.new
+    object = Book.new
     assert_equal("normal", object.type)
     assert_equal(15, object.number)
   end
 
   def test_default_value_order
-    Number.default_value_for :count, 5
-    Number.default_value_for :number do |this|
+    Book.default_value_for :count, 5
+    Book.default_value_for :number do |this|
       this.count * 2
     end
-    object = Number.new
+    object = Book.new
     assert_equal(5, object.count)
     assert_equal(10, object.number)
   end
 
   def test_attributes_with_default_values_are_not_marked_as_changed
-    Number.default_value_for :count, 5
-    Number.default_value_for :number, 2
+    Book.default_value_for :count, 5
+    Book.default_value_for :number, 2
 
-    object = Number.new
+    object = Book.new
     assert(!object.changed?)
     assert_equal([], object.changed)
 
@@ -408,10 +408,10 @@ class DefaultValuePluginTest < TestCaseClass
   end
 
   def test_constructor_does_not_affect_the_hash_passed_to_it
-    Number.default_value_for :count, 5
+    Book.default_value_for :count, 5
     options = { :count => 5, :user_id => 1 }
     options_dup = options.dup
-    Number.new(options)
+    Book.new(options)
     assert_equal(options_dup, options)
   end
 
@@ -419,18 +419,18 @@ class DefaultValuePluginTest < TestCaseClass
     define_model_class do
       default_value_for :number, 5678
     end
-    define_model_class("SpecialNumber", "TestClass")
-    n = SpecialNumber.create
-    assert SpecialNumber.find(n.id)
+    define_model_class("SpecialBook", "TestClass")
+    n = SpecialBook.create
+    assert SpecialBook.find(n.id)
   end
 
   def test_does_not_see_false_as_blank_at_boolean_columns_for_existing_records
-    Number.default_value_for(:flag, :allows_nil => false) { true }
+    Book.default_value_for(:flag, :allows_nil => false) { true }
 
-    object = Number.create
+    object = Book.create
 
     # allows nil for existing records
     object.update_attribute(:flag, false)
-    assert_equal false, Number.find(object.id).flag
+    assert_equal false, Book.find(object.id).flag
   end
 end
