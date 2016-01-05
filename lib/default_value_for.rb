4054e0836410230c540b3c64fc196b9eb3aead98
Alexandros Giouzenis <alexandrosg@gmail.com>
2016-01-05 19:16:50 +0200

    Fix for concurrency

--- lib/default_value_for.rb
+++ lib/default_value_for.rb
@@ -116,7 +116,7 @@ module DefaultValueFor
 
     def _all_default_attribute_values_not_allowing_nil
       return _default_attribute_values_not_allowing_nil unless superclass.respond_to?(:_default_attribute_values_not_allowing_nil)
-      result = superclass._all_default_attribute_values_not_allowing_nil.concat(_default_attribute_values_not_allowing_nil)
+      result = superclass._all_default_attribute_values_not_allowing_nil + _default_attribute_values_not_allowing_nil
       result.uniq!
       result
     end
