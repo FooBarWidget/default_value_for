FooBarWidget/default_value_for

commit 6f83fd6fe1d4a3b175c4529391ef3f8879bbb4eb

Fix stored attributes + allows_nil => false
Fix bug that prevented default value from working when a value was passed in at initialization, and `:allows_nil => false`. Only the attributes hash was being read, so stored attributes were not being detected.

Closes #59.

lib/default_value_for.rb
@@ -158,9 +158,9 @@ module DefaultValueFor
 
         column = self.class.columns.detect {|c| c.name == attribute}
         attribute_blank = if column && column.type == :boolean
-          attributes[attribute].nil?
+          send(attribute).nil?
         else
-          attributes[attribute].blank?
+          send(attribute).blank?
         end
         next unless connection_default_value_defined || attribute_blank
 

test.rb
@@ -49,6 +49,7 @@ ActiveRecord::Base.establish_connection(
 ActiveRecord::Base.connection.create_table(:users, :force => true) do |t|
   t.string :username
   t.integer :default_number
+  t.text :settings
 end
 
 ActiveRecord::Base.connection.create_table(:books, :force => true) do |t|
@@ -351,6 +352,14 @@ class DefaultValuePluginTest < TestCaseClass
 
     user = User.create! :books_attributes => [{:number => 1}]
     assert_equal 1, Book.all.first.number
+  end
+
+  def test_works_with_stored_attribute_accessors_when_initializing_value_that_does_not_allow_nil
+    User.store :settings, :accessors => :bio
+    User.default_value_for :bio, :value => 'None given', :allows_nil => false
+
+    user = User.create!(:bio => 'This is a bio')
+    assert_equal 'This is a bio', user.bio
   end
 
   if ActiveRecord::VERSION::MAJOR == 3

commit d155ba71776cb36308a4b73182b8068128b9fab8

Fix regression w/ writer and no reader
According to the README, this behavior is expected:
https://github.com/FooBarWidget/default_value_for/tree/1d1b2592eb00741a290d8406353846ddd8aae335#attributes-that-arent-database-columns

However, it fails after 6f83fd6fe1d4a3b175c4529391ef3f8879bbb4eb

This change allows all three types of scenarios that are expected:

* Normal attributes (usually backed by columns)
* Attributes not found within the attributes hash (ex: ActiveRecord::Store) but are accessed via a reader
* Attributes with *only* a writer and no reader (as in the README link above)

lib/default_value_for.rb
@@ -156,12 +156,18 @@ module DefaultValueFor
 
         connection_default_value_defined = new_record? && respond_to?("#{attribute}_changed?") && !__send__("#{attribute}_changed?")
 
-        column = self.class.columns.detect {|c| c.name == attribute}
-        attribute_blank = if column && column.type == :boolean
-          send(attribute).nil?
-        else
-          send(attribute).blank?
-        end
+        attribute_blank = if attributes.has_key?(attribute)
+                            column = self.class.columns.detect { |c| c.name == attribute }
+                            if column && column.type == :boolean
+                              attributes[attribute].nil?
+                            else
+                              attributes[attribute].blank?
+                            end
+                          elsif respond_to?(attribute)
+                            send(attribute).nil?
+                          else
+                            instance_variable_get("@#{attribute}").nil?
+                          end
         next unless connection_default_value_defined || attribute_blank
 
         # allow explicitly setting nil through allow nil option

test.rb
@@ -234,6 +234,14 @@ class DefaultValuePluginTest < TestCaseClass
     assert_equal 'hi', Book.new.hello
   end
 
+  def test_works_on_attributes_that_only_have_writers
+    Book.class_eval do
+      default_value_for :hello, "hi"
+      attr_writer :hello
+    end
+    assert_equal 'hi', Book.new.instance_variable_get('@hello')
+  end
+
   def test_doesnt_conflict_with_overrided_initialize_method_in_model_class
     Book.class_eval do
       def initialize(attrs = {})